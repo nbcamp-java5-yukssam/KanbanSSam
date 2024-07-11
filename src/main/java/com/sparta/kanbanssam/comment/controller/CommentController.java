ad5e238c9b2df87f301f79a81e77666f4a084e42
User <sonael121@naver.com>
2024-07-11 16:40:59 +0900

    feat: 댓글 생성
    comment.html, comment.js 추가

--- src/main/java/com/sparta/kanbanssam/comment/controller/CommentController.java
+++ src/main/java/com/sparta/kanbanssam/comment/controller/CommentController.java
@@ -8,22 +8,27 @@ import com.sparta.kanbanssam.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/cards/{cardId}/comment")
+    @PostMapping("/card/{cardId}/comment")
     public ResponseEntity<?> addComment(@PathVariable("cardId") Long cardId, @RequestBody CommentRequestDto requestDto) {
         // todo : secutity 구현 완료 시 userDetails 로 수정
         User user = User.builder().id(1L).build();
         CommentCreatedResponseDto responseDto = commentService.createComment(cardId, requestDto, user);
         return ResponseEntity.ok(responseDto);
     }
+
+    @GetMapping("/comment/view")
+    public String cardView() {return "/comment/comment";}
 }
