89b2899d8509047baaae89d38d9eef4d5760eb9e
User <sonael121@naver.com>
2024-07-11 14:58:01 +0900

    feat: 댓글 생성
    댓글 생성 기능 구현

---
+++ src/main/java/com/sparta/kanbanssam/comment/controller/CommentController.java
@@ -0,0 +1,29 @@
+package com.sparta.kanbanssam.comment.controller;
+
+
+import com.sparta.kanbanssam.comment.dto.CommentCreatedResponseDto;
+import com.sparta.kanbanssam.comment.dto.CommentRequestDto;
+import com.sparta.kanbanssam.comment.service.CommentService;
+import com.sparta.kanbanssam.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/cards/{cardId}/comment")
+    public ResponseEntity<?> addComment(@PathVariable("cardId") Long cardId, @RequestBody CommentRequestDto requestDto) {
+        // todo : secutity 구현 완료 시 userDetails 로 수정
+        User user = User.builder().id(1L).build();
+        CommentCreatedResponseDto responseDto = commentService.createComment(cardId, requestDto, user);
+        return ResponseEntity.ok(responseDto);
+    }
+}
