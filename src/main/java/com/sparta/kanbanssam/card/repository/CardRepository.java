b62198eb9862348fc9cd59857e1c78cee237661b
ydh5017 <ydh5017@naver.com>
2024-07-12 13:03:23 +0900

    feat: 카드 조회 기능 구현
    1. 단일 카드 조회
    2. 보드 별 전체 카드 목록 조회
    3. 단일 컬럼 카드 목록 조회
    3. 전체 컬럼 별 카드 목록 조회
    4. Board 엔티티 columnList, cardList 양방향 관계 추가
    5. 카드 조회 API 호출 HTTP Client 추가
    6. 로그인/회원가입 코드 병합
     - 각 도메인 컨트롤러 인증 객체 파라미터 추가
     - SecutiryConfig requestMatchers 추가
    
    resolves:#16

--- src/main/java/com/sparta/kanbanssam/card/repository/CardRepository.java
+++ src/main/java/com/sparta/kanbanssam/card/repository/CardRepository.java
@@ -1,8 +1,14 @@
 package com.sparta.kanbanssam.card.repository;
 
+import com.sparta.kanbanssam.card.dto.CardRequestDto;
 import com.sparta.kanbanssam.card.entity.Card;
+import com.sparta.kanbanssam.column.entity.Columns;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     Long countAllByColumnsId(Long columnId);
+
+    List<Card> findAllByColumnsOrderByOrders(Columns columns);
 }
